cd4b8e3711150fb736aa380655660c57907664fe
MBG henry <henry.ta@mindbank.tech>
2021-05-20 21:58:53 +0700

    Connect Chat

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -128,5 +128,17 @@
                 </div>
             </div>
         </div>
+        <script>
+            var conn = new WebSocket('ws://localhost:8080');
+            conn.onopen = function(e) {
+                console.log("Connection established!");
+                conn.send('Hello World!');
+
+            };
+
+            conn.onmessage = function(e) {
+                console.log(e.data);
+            };
+        </script>
     </body>
 </html>
